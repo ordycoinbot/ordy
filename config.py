51bc699512a4208c3ff1ece899fbed3590f34994
ORDINARY COIN <ordinarycoin@getordinary.website>
2025-06-22 16:28:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 TOKEN = '7646360181:AAFGYyYrJ51G3YLbBbGFhA2418puiDspXQI'
 
-   import os
+  TOKEN = '7646360181:AAFGYyYrJ51G3YLbBbGFhA2418puiDspXQI'
 
 db_config = {
-  "host":     os.getenv("MYSQL_HOST"),
-  "user":     os.getenv("MYSQL_USER"),
-  "password": os.getenv("MYSQL_PASSWORD"),
-  "database": os.getenv("MYSQL_DB"),
-  "port":     int(os.getenv("MYSQL_PORT", 3306)),
-}
-
+    'host': 'localhost',
+    'user': 'kineskirestoranb_b9271',
+    'password': 'B.K5HyA69JwRddVxy5N87',
+    'database': 'kineskirestoranb_b9271'
 }
 
 TELEGRAM_GROUP_ID = -1002385537018
+
+}
+
+
